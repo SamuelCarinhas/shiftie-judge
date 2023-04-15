fbf001c7eb1cc3a39ea23709e97b7b9a0fed2d8a
Samuel Carinhas <samuelsantos.c.2001@gmail.com>
2023-04-15 22:11:19 +0100

    Add file uploads

--- backend/src/routes/problem/problem.route.js
+++ backend/src/routes/problem/problem.route.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import Problem from "../../schemas/problem.schema.js";
+import uploadFile from '../../utils/multer.util.js';
 const router = express.Router();
 
 router.get('/list', (req, res) => {
@@ -24,6 +25,15 @@ router.get('/', (req, res) => {
                }
            ]
        }));
+});
+
+router.post('/submit', (req, res) => {
+    uploadFile(req, res, (err) => {
+        if(err)
+            return res.status(500).json({ errors: [ { msg: err } ] })
+        console.log(req.file);
+        res.status(200).json({ msg: 'File uploaded' } )
+    });
 });
 
 export default router;
